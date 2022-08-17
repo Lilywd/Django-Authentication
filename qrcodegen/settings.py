a19a8ffb99e960f4390e5abfdd0caf9af33d3f93
Lilywd <ndungulilianwanjiku@gmail.com>
2022-08-17 19:12:46 +0300

    'hardcodesecretkey'

--- qrcodegen/settings.py
+++ qrcodegen/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =env('SECRET_KEY')
+SECRET_KEY ='django-insecure-3zm3$6kcy1oathn@i2d02!l)@qj6r1^$^1auh=j2#pe6#+lh_n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
