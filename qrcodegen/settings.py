80cb6cb03dd50a21e7fd033e1858b88a10fa94d1
Lilywd <ndungulilianwanjiku@gmail.com>
2022-08-18 11:42:29 +0300

    'whitenoise'

--- qrcodegen/settings.py
+++ qrcodegen/settings.py
@@ -153,6 +153,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+# Enable WhiteNoise's GZip compression of static assets.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
