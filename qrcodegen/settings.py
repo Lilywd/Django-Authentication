b3466247974b6ac21b8131ab2f8330675ac0c178
Lilywd <ndungulilianwanjiku@gmail.com>
2022-08-20 23:13:07 +0300

    'database'

--- qrcodegen/settings.py
+++ qrcodegen/settings.py
@@ -104,8 +104,13 @@ WSGI_APPLICATION = 'qrcodegen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+
     }
 }
 # Password validation
